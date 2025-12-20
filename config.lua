d064eae041d672e5db654a1cf1f8555ce49f0e59
whentheh-yt <mkacper1985@gmail.com>
2025-12-20 13:37:23 +0000

    config.lua for December 2025 Build 0.1.14783

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ local config = {}
 
 config.game = {
     title = "Molvolution",
-    version = "December 2025 Build 0.1.14779",
+    version = "December 2025 Build 0.1.14783",
     window = {
         width = 1200,
         height = 800,
